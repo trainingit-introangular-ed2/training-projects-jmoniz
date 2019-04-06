18f570f46b51495bdca5859ed3e1946111bdf79a
José L. Moñiz <jluque@hanuman.local>
2019-04-06 14:31:58 +0200

    Add new components.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FooterComponent } from './shell/footer/footer.component';
+import { HeaderComponent } from './shell/header/header.component';
+import { MainComponent } from './shell/main/main.component';
+import { ShellComponent } from './shell/shell.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ShellComponent, HeaderComponent, MainComponent, FooterComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [ShellComponent]
 })
-export class CoreModule { }
+export class CoreModule {}
