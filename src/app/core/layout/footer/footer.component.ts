a86d064b24e7e5c123e563adcfb1ddfdf5178a59
José L. Moñiz <jluque@hanuman.local>
2019-04-13 19:34:48 +0200

    Added HomeModule and Project counter

--- src/app/core/layout/footer/footer.component.ts
+++ src/app/core/layout/footer/footer.component.ts
@@ -6,10 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  public currentDate = new Date();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,13 @@
 
 export const environment = {
   production: false,
-  appName: 'Angular Board (DEV)'
+  appName: 'Angular Board (DEV)',
+  projects: [
+    { id: 0, name: 'Learn Angular' },
+    { id: 1, name: 'Develop My Dream app' },
+    { id: 2, name: 'Travel around the world' },
+    { id: 3, name: 'Clean my room' }
+  ]
 };
 
 /*
