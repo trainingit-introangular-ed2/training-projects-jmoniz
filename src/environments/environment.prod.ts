da7b6683dc8aa10417abe93462f649b47fecd25f
José L. Moñiz <jluque@hanuman.local>
2019-05-02 18:59:18 +0200

    Remove projecct array from environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,4 @@
 export const environment = {
   production: true,
-  appName: 'Angular Board',
-  projects: [
-    { id: 0, name: 'Learn Angular' },
-    { id: 1, name: 'Develop My Dream app' },
-    { id: 2, name: 'Travel around the world' },
-    { id: 3, name: 'Clean my room' }
-  ]
+  appName: 'Angular Board'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,7 @@
 
 export const environment = {
   production: false,
-  appName: 'Angular Board (DEV)',
-  projects: [
-    { id: 0, name: 'Learn Angular' },
-    { id: 1, name: 'Develop My Dream app' },
-    { id: 2, name: 'Travel around the world' },
-    { id: 3, name: 'Clean my room' }
-  ]
+  appName: 'Angular Board (DEV)'
 };
 
 /*
